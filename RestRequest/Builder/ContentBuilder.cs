c366fae687d2b70d6a96e6acba8e5bf6a0daac03
wangzelin <315872858@qq.com>
2018-09-28 18:59:32 +0800

    调整代码结构

---
+++ RestRequest/Builder/ContentBuilder.cs
@@ -0,0 +1,431 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestRequest.Body;
+using RestRequest.Interface;
+
+namespace RestRequest.Builder
+{
+	public class ContentBuilder : BaseBuilder, IBodyBuilder
+	{
+
+		internal ContentBuilder(string url, HttpMethod method) : base(url, method)
+		{
+		}
+
+		public IActionCallback OnSuccess(Action<HttpStatusCode, Stream> action, HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			SuccessAction = action;
+			SucceedStatus = succeedStatus;
+			return this;
+		}
+
+		public IActionCallback OnSuccess(Action<HttpStatusCode, string> action, HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			SuccessAction = (statusCode, stream) =>
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					action(statusCode, reader.ReadToEnd());
+				}
+			};
+			SucceedStatus = succeedStatus;
+			return this;
+		}
+
+		public IActionCallback OnSuccess<T>(Action<HttpStatusCode, T> action, HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			SuccessAction = (statusCode, stream) =>
+			{
+				using (var reader = new StreamReader(stream))
+				{
+					action(statusCode, JsonConvert.DeserializeObject<T>(reader.ReadToEnd()));
+				}
+			};
+			SucceedStatus = succeedStatus;
+			return this;
+		}
+
+		public IActionCallback OnFail(Action<HttpStatusCode?, string> action, HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			FailAction = action;
+			SucceedStatus = succeedStatus;
+			return this;
+		}
+
+		public void Start()
+		{
+			var builder = new RequestBuilder(this);
+			builder.BuildRequest();
+			builder.BuildCallback();
+		}
+
+		public ResponseResult<Stream> ResponseStream(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			using (var builder = new RequestBuilder(this))
+			{
+				builder.BuildRequest();
+				builder.WriteRequestBody();
+				var res = builder.GetResponse();
+				var succeed = res.StatusCode == succeedStatus;
+				var contentStream = res.GetResponseStream();
+				var contentString = "";
+				if (!succeed && contentStream != null)
+				{
+					using (var reader = new StreamReader(contentStream))
+					{
+						contentString = reader.ReadToEnd();
+					}
+				}
+				return new ResponseResult<Stream>
+				{
+					Succeed = succeed,
+					StatusCode = res.StatusCode,
+					Content = contentStream,
+					FailedContent = contentString,
+					Response = res,
+					Request = builder.Request
+				};
+			}
+		}
+
+		public async Task<ResponseResult<Stream>> ResponseStreamAsync(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			using (var builder = new RequestBuilder(this))
+			{
+				builder.BuildRequest();
+				await builder.WriteRequestBodyAsync();
+				var res = await builder.GetResponseAsync();
+				var succeed = res.StatusCode == succeedStatus;
+				var contentStream = res.GetResponseStream();
+				var contentString = "";
+				if (!succeed && contentStream != null)
+				{
+					using (var reader = new StreamReader(contentStream))
+					{
+						contentString = await reader.ReadToEndAsync();
+					}
+				}
+				return new ResponseResult<Stream>
+				{
+					Succeed = succeed,
+					StatusCode = res.StatusCode,
+					Content = contentStream,
+					FailedContent = contentString,
+					Response = res,
+					Request = builder.Request
+				};
+			}
+		}
+
+		public ResponseResult<string> ResponseString(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			var res = ResponseStream();
+			using (var stream = res.Content)
+			using (var reader = new StreamReader(stream))
+			using (res.Response)
+			{
+				return new ResponseResult<string>
+				{
+					Succeed = res.StatusCode == succeedStatus,
+					StatusCode = res.StatusCode,
+					Content = reader.ReadToEnd(),
+					Response = res.Response,
+					Request = res.Request
+				};
+			}
+		}
+
+		public async Task<ResponseResult<string>> ResponseStringAsync(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			var res = await ResponseStreamAsync();
+			using (var stream = res.Content)
+			using (var reader = new StreamReader(stream))
+			using (res.Response)
+			{
+				return new ResponseResult<string>
+				{
+					Succeed = res.StatusCode == succeedStatus,
+					StatusCode = res.StatusCode,
+					Content = await reader.ReadToEndAsync(),
+					Response = res.Response,
+					Request = res.Request
+				};
+			}
+		}
+
+		public ResponseResult<T> ResponseValue<T>(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			var res = ResponseString();
+			return new ResponseResult<T>
+			{
+				Succeed = res.StatusCode == succeedStatus,
+				StatusCode = res.StatusCode,
+				Content = string.IsNullOrWhiteSpace(res.Content) ? default : JsonConvert.DeserializeObject<T>(res.Content),
+				Response = res.Response,
+				Request = res.Request
+			};
+		}
+
+		public async Task<ResponseResult<T>> ResponseValueAsync<T>(HttpStatusCode succeedStatus = HttpStatusCode.OK)
+		{
+			var res = await ResponseStringAsync();
+			return new ResponseResult<T>
+			{
+				Succeed = res.StatusCode == succeedStatus,
+				StatusCode = res.StatusCode,
+				Content = string.IsNullOrWhiteSpace(res.Content) ? default : JsonConvert.DeserializeObject<T>(res.Content),
+				Response = res.Response,
+				Request = res.Request
+			};
+		}
+
+
+		public INoneBodyBuilder Headers(Dictionary<string, string> headers)
+		{
+			if (!(headers?.Count > 0)) return this;
+			foreach (var item in headers)
+				RequestHeaders[item.Key] = item.Value;
+			return this;
+		}
+
+		public INoneBodyBuilder Headers(object headers)
+		{
+			if (headers == null)
+				return this;
+			var properties = headers.GetType().GetProperties();
+			foreach (var item in properties)
+				RequestHeaders[item.Name] = item.GetValue(headers).ToString();
+			return this;
+		}
+
+		public INoneBodyBuilder AddCertificate(string certificateUrl, string certificatePassword)
+		{
+			if (!File.Exists(certificateUrl))
+				throw new FileNotFoundException($"证书文件不存在{certificateUrl}");
+			if (ClientCertificates == null)
+				ClientCertificates = new X509CertificateCollection();
+			ClientCertificates.Add(new X509Certificate(certificateUrl, certificatePassword));
+			return this;
+		}
+
+		public INoneBodyBuilder AddCertificate(byte[] rawData, string certificatePassword)
+		{
+			if (ClientCertificates == null)
+				ClientCertificates = new X509CertificateCollection();
+			ClientCertificates.Add(new X509Certificate(rawData, certificatePassword));
+			return this;
+		}
+
+		public INoneBodyBuilder AddCertificate(X509Certificate cert)
+		{
+			if (ClientCertificates == null)
+				ClientCertificates = new X509CertificateCollection();
+			ClientCertificates.Add(new X509Certificate(cert));
+			return this;
+		}
+
+		public INoneBodyBuilder IgnoreCertError()
+		{
+			IgnoreCertificateError = true;
+			return this;
+		}
+
+		public INoneBodyBuilder KeepAlive()
+		{
+			base.KeepAlive = true;
+			return this;
+		}
+
+		public INoneBodyBuilder UserAgent(string userAgent)
+		{
+			if (!string.IsNullOrWhiteSpace(userAgent))
+				base.UserAgent = userAgent;
+			return this;
+		}
+
+		public INoneBodyBuilder Referer(string referer)
+		{
+			base.Referer = referer;
+			return this;
+		}
+
+		public INoneBodyBuilder Timeout(int timeout)
+		{
+			if (timeout <= 0)
+				throw new ArgumentOutOfRangeException("超时时间必须大于0");
+			base.Timeout = timeout;
+			return this;
+		}
+
+		public INoneBodyBuilder Cookies(object cookies)
+		{
+			if (cookies == null) return this;
+			base.Cookies = new List<Cookie>();
+			var properties = cookies.GetType().GetProperties();
+			foreach (var enumerator in properties)
+			{
+				base.Cookies.Add(new Cookie { Name = enumerator.Name, Value = enumerator.GetValue(cookies).ToString(), Domain = Url.Host });
+			}
+			return this;
+		}
+
+		public INoneBodyBuilder Cookies(Dictionary<string, string> cookies)
+		{
+			if (cookies != null && cookies.Count > 0)
+			{
+				base.Cookies = new List<Cookie>();
+				foreach (var cookie in cookies)
+				{
+					base.Cookies.Add(new Cookie { Name = cookie.Key, Value = cookie.Value, Domain = Url.Host });
+				}
+			}
+			return this;
+		}
+
+		public INoneBodyBuilder Cookies(IEnumerable<Cookie> cookies)
+		{
+			base.Cookies = cookies?.ToList();
+			return this;
+		}
+
+		public INoneBodyBuilder ContentType(string contentType)
+		{
+			if (!string.IsNullOrWhiteSpace(contentType))
+				base.ContentType = contentType;
+			return this;
+		}
+
+		/// <summary>
+		/// 设置json数据
+		/// </summary>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Body(object parameters)
+		{
+			var body = new JsonBody();
+			body.AddParameter(parameters);
+			RequestBody = body;
+			ContentType("application/json");
+			return this;
+		}
+
+		/// <summary>
+		/// 设置表单数据
+		/// content-type=application/x-www-form-urlencoded
+		/// </summary>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(object parameters)
+		{
+			var body = new FormBody();
+			body.AddParameter(parameters);
+			RequestBody = body;
+
+			return this;
+		}
+
+		/// <summary>
+		/// 设置表单数据
+		/// content-type=application/x-www-form-urlencoded
+		/// </summary>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(Dictionary<string, object> parameters)
+		{
+			var body = new FormBody();
+			body.AddParameter(parameters);
+			RequestBody = body;
+			ContentType("application/x-www-form-urlencoded");
+			return this;
+		}
+
+		/// <summary>
+		/// 设置表单数据和上传的文件
+		/// content-type=multipart/form-data
+		/// </summary>
+		/// <param name="files">上传的文件信息</param>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(IEnumerable<NamedFileStream> files, object parameters)
+		{
+			var body = new MultipartBody();
+			body.AddParameters(parameters);
+			body.AddFiles(files);
+			RequestBody = body;
+			ContentType("multipart/form-data; boundary=" + body.Boundary);
+			return this;
+		}
+
+		/// <summary>
+		/// 设置上传的文件
+		/// content-type=multipart/form-data
+		/// </summary>
+		/// <param name="files">上传的文件信息</param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(IEnumerable<NamedFileStream> files)
+		{
+			var body = new MultipartBody();
+			body.AddFiles(files);
+			RequestBody = body;
+			ContentType("multipart/form-data; boundary=" + body.Boundary);
+			return this;
+		}
+
+		/// <summary>
+		/// 设置表单数据和上传的文件
+		/// content-type=multipart/form-data
+		/// </summary>
+		/// <param name="files">上传的文件信息</param>
+		/// <param name="parameters"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(IEnumerable<NamedFileStream> files, Dictionary<string, object> parameters)
+		{
+			var body = new MultipartBody();
+			body.AddParameters(parameters);
+			body.AddFiles(files);
+			RequestBody = body;
+			ContentType("multipart/form-data; boundary=" + body.Boundary);
+			return this;
+		}
+
+		/// <summary>
+		/// 设置提交的文本
+		/// content-type=application/text
+		/// </summary>
+		/// <param name="text">提交文本</param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(string text)
+		{
+			if (string.IsNullOrWhiteSpace(text))
+				throw new NullReferenceException("Post文本内容不能为空");
+			RequestBody = new TextBody(text);
+			ContentType("application/text");
+			return this;
+		}
+
+		/// <summary>
+		/// 设置提交的Stream
+		/// content-type=application/octet-stream
+		/// </summary>
+		/// <param name="stream"></param>
+		/// <returns></returns>
+		public INoneBodyBuilder Form(Stream stream)
+		{
+			if (stream == null)
+				throw new ArgumentNullException("The stream is null");
+			RequestBody = new StreamBody(stream);
+			ContentType("application/octet-stream");
+			return this;
+		}
+
+
+	}
+}
