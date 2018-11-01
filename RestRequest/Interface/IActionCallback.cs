5c38fb64c8a98f7188e52ce5981cc883058bb5a0
wangzelin <315872858@qq.com>
2018-11-01 17:39:49 +0800

    add download

--- RestRequest/Interface/IActionCallback.cs
+++ RestRequest/Interface/IActionCallback.cs
@@ -6,35 +6,17 @@ namespace RestRequest.Interface
 {
 	public interface IActionCallback
 	{
-		/// <summary>
-		/// 成功时回调
-		/// </summary>
-		/// <param name="action"></param>
-		/// <param name="succeedStatus"></param>
-		/// <returns></returns>
 		IActionCallback OnSuccess(Action<HttpStatusCode, Stream> action, HttpStatusCode succeedStatus = HttpStatusCode.OK);
 
-		/// <summary>
-		/// 成功时回调
-		/// </summary>
-		/// <param name="action"></param>
-		/// <param name="succeedStatus"></param>
-		/// <returns></returns>
+
 		IActionCallback OnSuccess(Action<HttpStatusCode, string> action, HttpStatusCode succeedStatus = HttpStatusCode.OK);
 
 		IActionCallback OnSuccess<T>(Action<HttpStatusCode, T> action, HttpStatusCode succeedStatus = HttpStatusCode.OK);
 
-		/// <summary>
-		/// 失败时回调
-		/// </summary>
-		/// <param name="action"></param>
-		/// <param name="succeedStatus"></param>
-		/// <returns></returns>
+
 		IActionCallback OnFail(Action<HttpStatusCode?, string> action, HttpStatusCode succeedStatus = HttpStatusCode.OK);
 
-		/// <summary>
-		/// 开始请求
-		/// </summary>
+
 		void Start();
 	}
 }
--- RestRequest/Interface/IBodyBuilder.cs
+++ RestRequest/Interface/IBodyBuilder.cs
@@ -5,70 +5,26 @@ namespace RestRequest.Interface
 {
 	public interface IBodyBuilder : INoneBodyBuilder
 	{
-		/// <summary>
-		/// 提交json数据
-		/// Content-Type=application/json
-		/// </summary>
-		/// <param name="parameters"></param>
-		/// <returns></returns>
 		INoneBodyBuilder Body(object parameters);
 
-		/// <summary>
-		/// Post模拟提交表单数据
-		/// Content-Type=application/x-www-form-urlencoded
-		/// </summary>
-		/// <param name="parameters"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(object parameters);
 
-		/// <summary>
-		/// Post模拟提交表单数据
-		/// Content-Type=application/x-www-form-urlencoded
-		/// </summary>
-		/// <param name="parameters"></param>
-		/// <returns></returns>
 		INoneBodyBuilder Form(IDictionary<string, object> parameters);
 
-		/// <summary>
-		/// 同时上传文件和表单数据
-		/// Content-Type=multipart/form-data
-		/// </summary>
-		/// <param name="files"></param>
-		/// <param name="parameters"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(IEnumerable<NamedFileStream> files, IDictionary<string, object> parameters);
 
-		/// <summary>
-		/// 同时上传文件和表单数据
-		/// Content-Type=multipart/form-data
-		/// </summary>
-		/// <param name="files"></param>
-		/// <param name="parameters"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(IEnumerable<NamedFileStream> files, object parameters);
 
-		/// <summary>
-		/// 上传文件
-		/// Content-Type=multipart/form-data
-		/// </summary>
-		/// <param name="files"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(IEnumerable<NamedFileStream> files);
 
-		/// <summary>
-		/// 提交文本
-		/// Content-Type=application/text
-		/// </summary>
-		/// <param name="text"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(string text);
 
-		/// <summary>
-		/// 提交Stream
-		/// Content-Type=application/octet-stream
-		/// </summary>
-		/// <param name="stream"></param>
-		/// <returns></returns>
+
 		INoneBodyBuilder Form(Stream stream);
 	}
 }
